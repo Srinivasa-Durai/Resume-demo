e464c55c32790f86225e46796416a775a1474dae
Srinivasa-Durai <118293627+Srinivasa-Durai@users.noreply.github.com>
2022-11-28 19:01:28 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { PersonalModal, EducationModal, WorkModal } from "./modal";
+
+const Index = () => {
+    return (
+        <div>Index
+            <PersonalModal />
+            <EducationModal />
+            <WorkModal />
+        </div>
+    )
+}
+
+export default Index;
